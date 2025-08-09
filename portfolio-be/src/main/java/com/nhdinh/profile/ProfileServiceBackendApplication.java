7a9f72fd2a93bf330e4262c74965332a2c14177c
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-08-09 14:05:06 +0700

    tao moi truong du an

---
+++ portfolio-be/src/main/java/com/nhdinh/profile/ProfileServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nhdinh.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileServiceBackendApplication.class, args);
+	}
+
+}
---
+++ portfolio-be/src/test/java/com/nhdinh/profile/ProfileServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhdinh.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

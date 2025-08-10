844d766555403cd33efc7ab20b0a1e397143d63f
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-08-10 13:20:24 +0700

    thiết lập tối ưu file chuyên nghiệp

--- portfolio-fe/src/App.jsx
+++ portfolio-fe/src/App.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Experience from './pages/Experience';
-import Contact from './pages/Contact';
-import './styles/App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>Đây là file App.jsx mặc định, không phụ thuộc vào file khác.</p>
+    </div>
   );
 }
 

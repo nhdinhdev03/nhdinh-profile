d1718daa2a21720c94984eac28d0ee9560603151
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-08-12 00:57:56 +0700

    icon và hinh anh

---
+++ portfolio-fe/src/assets/Img/index.js
@@ -0,0 +1,13 @@
+import logoAdmin from "./logo_Admin.png";
+import Co_VN from "./vietnam-flag.png";
+import Co_My from "./us-flag.png";
+
+export const listIcon = {};
+
+const img = {
+  logoAdmin,
+  Co_VN,
+  Co_My,
+};
+
+export default img;

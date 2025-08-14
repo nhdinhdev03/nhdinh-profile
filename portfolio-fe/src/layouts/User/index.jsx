a382339c7650ecbf4d3b3c0242f4d94a67172ce9
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-08-14 21:14:46 +0700

    tối ưu Breadcrumb

--- portfolio-fe/src/layouts/User/index.jsx
+++ portfolio-fe/src/layouts/User/index.jsx
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Breadcrumb from "../Breadcrumb";
 import Footer from "./Footer";
 import { ROUTES } from "router/routeConstants";
+import "./UserLayout.scss";
 
 function UserLayout({ children }) {
   const location = useLocation();
@@ -13,7 +14,7 @@ function UserLayout({ children }) {
     <div className="layout">
       <Header />
 
-      <main id="main" className="page-container">
+      <main id="main" className={`page-container ${isHomePage ? "home-container" : ""}`}>
         {!isHomePage && (
           <div className="px-4 sm:px-6 lg:px-8 py-2 max-w-[1280px] mx-auto w-full">
             <Breadcrumb />

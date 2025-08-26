a267bc3599edd767a85862e9112549b07c67ff31
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-08-26 21:42:29 +0700

    tối ưu xóa bò file

--- portfolio-fe/src/components/UI/Loading/index.jsx
+++
@@ -1,6 +0,0 @@
-import LoadingSpinner from './LoadingSpinner';
-import LoadingPage from './LoadingPage';
-import LoadingOverlay from './LoadingOverlay';
-
-export { LoadingSpinner, LoadingPage, LoadingOverlay };
-export default LoadingSpinner;

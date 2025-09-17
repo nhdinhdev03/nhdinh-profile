8ca6a1b6ac22a149903f8b901326855246b6b266
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-09-18 00:47:53 +0700

    s

--- portfolio-fe/src/App.jsx
+++ portfolio-fe/src/App.jsx
@@ -12,8 +12,9 @@ import ScrollToTopOnNavigate from "components/Scroll/ScrollToTopOnNavigate/Scrol
 import "styles/App.scss";
 
 
-import NotFound from "pages/NotFound/index";
+
 import "./i18n";
+import NotFound from "./Pages/NotFound";
 
 const App = memo(() => {
   // Không cần truyền initialTheme vì useTheme đã tự động đọc từ document/localStorage
